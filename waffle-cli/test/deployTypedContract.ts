c0105902ee266fbbe28e1676d1463860201a5d02
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-09-08 10:42:03 +1000

    chore: update author and homepage in package.json

--- waffle-cli/test/deployTypedContract.ts
+++ waffle-cli/test/deployTypedContract.ts
@@ -1,16 +1,17 @@
-import {parseEther} from 'ethers/lib/utils';
-import {BasicTokenMock, BasicTokenMock__factory} from '../build';
-import {deployContract, MockProvider} from '../src';
-import {expect} from 'chai';
+import { parseEther } from "ethers/lib/utils";
+import { BasicTokenMock, BasicTokenMock__factory } from "../build";
+import { deployContract, MockProvider } from "../src";
+import { expect } from "chai";
 
-describe('INTEGRATION: deployTypedContract', () => {
+describe("INTEGRATION: deployTypedContract", () => {
   const [wallet] = new MockProvider().getWallets();
 
-  it('successfully deploys contract', async () => {
-    const contract: BasicTokenMock = await deployContract(wallet,
+  it("successfully deploys contract", async () => {
+    const contract: BasicTokenMock = await deployContract(
+      wallet,
       BasicTokenMock__factory,
-      [wallet.address, parseEther('100')]
+      [wallet.address, parseEther("100")]
     );
-    expect(await contract.balanceOf(wallet.address)).to.eq(parseEther('100'));
+    expect(await contract.balanceOf(wallet.address)).to.eq(parseEther("100"));
   });
 });
