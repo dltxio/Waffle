fd279effb1579453a8ce2c2d6e5fca8a0a963ce6
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-09-08 18:17:30 +1000

    Refactor import statements in waffle-compiler/src/compileSolcjs.ts

--- waffle-compiler/src/compileSolcjs.ts
+++ waffle-compiler/src/compileSolcjs.ts
@@ -1,28 +1,31 @@
-import solc from 'solc';
-import path from 'path';
-import fetch from 'node-fetch';
-import {ImportFile} from '@resolver-engine/imports';
-import {isDirectory, isFile, removeEmptyDirsRecursively} from './utils';
-import {Config} from './config';
-import {getCompilerInput} from './compilerInput';
-import {findImports} from './findImports';
-import mkdirp from 'mkdirp';
-import fs from 'fs';
-import https from 'https';
+import solc from "solc";
+import path from "path";
+import fetch from "node-fetch";
+import { ImportFile } from "@resolver-engine/imports";
+import { isDirectory, isFile, removeEmptyDirsRecursively } from "./utils";
+import { Config } from "./config";
+import { getCompilerInput } from "./compilerInput";
+import { findImports } from "./findImports";
+import mkdirp from "mkdirp";
+import fs from "fs";
+import https from "https";
 
 const semverRegex = /^\d+\.\d+\.\d+$/;
 
 export function compileSolcjs(config: Config) {
   return async function compile(sources: ImportFile[]) {
     const solc = await loadCompiler(config);
-    const input = getCompilerInput(sources, config.compilerOptions, 'Solidity');
+    const input = getCompilerInput(sources, config.compilerOptions, "Solidity");
     const imports = findImports(sources);
-    const output = solc.compile(input, {imports});
+    const output = solc.compile(input, { imports });
     return JSON.parse(output);
   };
 }
 
-export async function loadCompiler({compilerVersion, cacheDirectory}: Config) {
+export async function loadCompiler({
+  compilerVersion,
+  cacheDirectory,
+}: Config) {
   if (isDefaultVersion(compilerVersion)) {
     return solc;
   }
@@ -40,30 +43,36 @@ export async function loadCompiler({compilerVersion, cacheDirectory}: Config) {
 }
 
 function isDefaultVersion(version: string) {
-  return version === 'default' ||
-    (semverRegex.test(version) && solc.version().startsWith(`${version}+`));
+  return (
+    version === "default" ||
+    (semverRegex.test(version) && solc.version().startsWith(`${version}+`))
+  );
 }
 
 async function resolveSemverVersion(version: string) {
   const releases = await fetchReleases();
   const item: string = releases[version];
-  return item.substring('soljson-'.length, item.length - '.js'.length);
+  return item.substring("soljson-".length, item.length - ".js".length);
 }
 
-const VERSION_LIST_URL = 'https://solc-bin.ethereum.org/bin/list.json';
+const VERSION_LIST_URL = "https://solc-bin.ethereum.org/bin/list.json";
 let cache: any = undefined;
 
 async function fetchReleases() {
   if (!cache) {
     const res = await fetch(VERSION_LIST_URL);
-    const {releases} = await res.json();
+    const { releases } = (await res.json()) as { releases: any };
     cache = releases;
   }
   return cache;
 }
 
 async function loadVersion(version: string, cacheDirectory: string) {
-  const cachedSolcPath = path.resolve(cacheDirectory, 'solcjs', `${version}.js`);
+  const cachedSolcPath = path.resolve(
+    cacheDirectory,
+    "solcjs",
+    `${version}.js`
+  );
   if (!isFile(cachedSolcPath)) {
     await cacheRemoteVersion(version, cacheDirectory);
   }
@@ -71,7 +80,7 @@ async function loadVersion(version: string, cacheDirectory: string) {
 }
 
 async function cacheRemoteVersion(version: string, cacheDirectory: string) {
-  const solcCacheDirectory = path.resolve(cacheDirectory, 'solcjs');
+  const solcCacheDirectory = path.resolve(cacheDirectory, "solcjs");
   if (!isDirectory(solcCacheDirectory)) {
     mkdirp.sync(solcCacheDirectory);
   }
@@ -81,20 +90,22 @@ async function cacheRemoteVersion(version: string, cacheDirectory: string) {
   const url = `https://solc-bin.ethereum.org/bin/soljson-${version}.js`;
 
   await new Promise<void>((resolve, reject) => {
-    https.get(url, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
-        resolve();
+    https
+      .get(url, (response) => {
+        response.pipe(file);
+        file.on("finish", () => {
+          file.close();
+          resolve();
+        });
+      })
+      .on("error", (error) => {
+        try {
+          fs.unlinkSync(filePath);
+          removeEmptyDirsRecursively(path.resolve(cacheDirectory));
+        } finally {
+          reject(error);
+        }
       });
-    }).on('error', (error) => {
-      try {
-        fs.unlinkSync(filePath);
-        removeEmptyDirsRecursively(path.resolve(cacheDirectory));
-      } finally {
-        reject(error);
-      }
-    });
   });
 }
 
